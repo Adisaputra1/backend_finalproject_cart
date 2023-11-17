4247255b6e2517923beaa896e5cd3eab0ed991f4
rahmadadi <rahmadadisaputra12@gmail.com>
2023-11-17 08:22:09 +0700

    commit push cart

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.SERVER_PORT || 3000;
+
+// Router
+const authRouter = require('./app/auth/router');
+const categoryRouter = require('./app/category/router');
+const itemRoute = require('./app/items/Items_route');
+const orderRouter = require('./app/transaction/order/router');
+const orderDetailRouter = require('./app/transaction/detailOrder/router');
+const cartRoute = require('./app/cart/Carts_route');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Version
+const version = '/api/v1';
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// User / Auth
+app.use(`${version}/auth`, authRouter);
+
+// category
+app.use('/categories', categoryRouter);
+
+// items
+app.use('/items', itemRoute);
+
+//cart
+app.use('/cart', cartRoute);
+
+//transaction
+app.use(version, orderRouter);
+app.use(version, orderDetailRouter);
+
+app.listen(port, () => {
+  console.log(`Server running in http://localhost:${port}`);
+});
